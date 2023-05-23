0d30d6be2144ecd1b81be585cbdc6cedb48237ef
Curtis Turk <curtis.c.turk@outlook.com>
2023-05-23 17:48:34 +0100

    Pie chart implemented in chart.js

---
+++ app/chartJS/BarChart.tsx
@@ -0,0 +1,5 @@
+import { Bar } from "react-chartjs-2";
+
+function BarChart({ chartData }: { chartData: any }) {}
+
+export default BarChart;
--- app/chartJS/page.tsx
+++ app/chartJS/page.tsx
@@ -26,7 +26,7 @@ export default function ChartJS() {
         data: UserData.map((data) => data.userGain),
         backgroundColor: [
           "rgba(75,192,192,1)",
-          "ecf0f1",
+          "#2a16d0",
           "#50AF95",
           "#f3ba2f",
           "#2a71d0",
@@ -40,6 +40,7 @@ export default function ChartJS() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Chart.js</h1>
+      <PieChart chartData={chartData} />
     </main>
   );
 }
