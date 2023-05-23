c4a700848f235477bbdbf18a6882f378ab706124
Curtis Turk <curtis.c.turk@outlook.com>
2023-05-23 17:55:41 +0100

    Added a chart data type:

--- app/chartJS/PieChart.tsx
+++ app/chartJS/PieChart.tsx
@@ -1,6 +1,17 @@
 import { Pie } from "react-chartjs-2";
 
-function PieChart({ chartData }: { chartData: any }) {
+type ChartData = {
+  labels: number[];
+  datasets: {
+    label: string;
+    data: number[];
+    backgroundColor: string[];
+    borderColor: string;
+    borderWidth: number;
+  }[];
+};
+
+function PieChart({ chartData }: { chartData: ChartData }) {
   return (
     <div className="chart-container">
       <h2 style={{ textAlign: "center" }}>Pie Chart</h2>
