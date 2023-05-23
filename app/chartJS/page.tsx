79e4c5e0df927e01d0347f10d9e99da9b8031bf8
Curtis Turk <curtis.c.turk@outlook.com>
2023-05-23 17:42:51 +0100

    Added some initial code for creating a piechart

--- app/chartJS/page.tsx
+++ app/chartJS/page.tsx
@@ -1,4 +1,42 @@
+"use client";
+
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { UserData } from "@/utils/userData";
+import PieChart from "./PieChart";
+
+type UserDataObject = {
+  id: number;
+  year: number;
+  userGain: number;
+  userLost: number;
+};
+
+type UserDataObjects = UserDataObject[];
+
 export default function ChartJS() {
+  Chart.register(CategoryScale);
+
+  const [chartData, setChartData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Chart.js</h1>
