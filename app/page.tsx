18a2488742d3ee802ea080c186961cec9812f184
Curtis Turk <curtis.c.turk@outlook.com>
2023-05-23 17:11:05 +0100

    Added navigation to layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Graphs</h1>
+    <main className="flex min-h-screen items-center justify-between p-24">
       <Link href="/chartJS">Chart.js</Link>
     </main>
   );
